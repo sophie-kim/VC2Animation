57c4080ec78842d3263d012e3d3a28b514e4e5c5
Sophie Kim <sophiekim@carolines-mbp-2.attlocal.net>
2018-03-25 00:42:28 -0700

    functionalgit add -A

--- javascript.js
+++ javascript.js
@@ -20,5 +20,36 @@ $("#btn2").click(function () {
     showText('13', 5);
     showText('14', 6);
 });
-
-
+$("#btn3").click(function () {
+    showText('15', 1);
+    showText('16', 2);
+    showText('17', 3);
+    showText('18', 4);
+    showText('19', 5);
+});
+$("#btn4").click(function () {
+    showText('20', 1);
+    showText('21', 2);
+    showText('22', 3);
+    showText('23', 4);
+    showText('24', 5);
+});
+$("#btn5").click(function () {
+    showText('25', 1);
+    showText('26', 2);
+    showText('27', 3);
+});
+$("#btn6").click(function () {
+    showText('28', 1);
+    showText('29', 2);
+    showText('30', 3);
+    showText('31', 4);
+    showText('32', 5);
+});
+$("#btn7").click(function () {
+    showText('33', 1);
+    showText('34', 2);
+    showText('35', 3);
+    showText('36', 4);
+    showText('37', 5);
+});
