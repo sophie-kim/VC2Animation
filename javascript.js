a71da6b6ebc82e2ade9bff19fd777f752bd6671c
Sophie Kim <sophiekim@carolines-mbp-2.attlocal.net>
2018-03-25 00:24:40 -0700

    working load

--- javascript.js
+++ javascript.js
@@ -1,2 +1,24 @@
-$(document).ready(function () {
-    $("p").css("opacity","0")  
+function showText(id,delay){
+    var elem=document.getElementById(id);
+    setTimeout(function(){elem.style.opacity=1;},delay*1000)
+  }
+window.onload = function () {
+    showText('1', 1);
+    showText('2', 2);
+    showText('3', 3);
+    showText('4', 4);
+    showText('5', 5);
+    showText('6', 6);
+    showText('7', 7);
+    showText('8', 8);
+};
+$("#btn2").click(function () {
+    showText('9', 1);
+    showText('10', 2);
+    showText('11', 3);
+    showText('12', 4);
+    showText('13', 5);
+    showText('14', 6);
+});
+
+
